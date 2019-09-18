b4c9d99583acf8fbacff4ec75b34e0f04694d53e
cyberhafid <cyberhafid@gmail.com>
2019-09-18 21:59:07 +0200

    ajout route node

--- nodeback/routes/indices.js
+++ nodeback/routes/indices.js
@@ -16,10 +16,39 @@ router.get('/', function (req, res, next) {
   elastic.search(req.params.input).then(function (result) { res.json(result) });
 });
 
+
 /* POST document to be indexed */
 router.post('/test', function (req, res, next) {  
   elastic.addDocument(req.body).then(function (result) { res.json(result) });
 });
+
+
+router.get('/search', function (req, res){
+  // declare the query object to search elastic search and return only 200 results from the first result found. 
+  // also match any data where the name is like the query string sent in
+  let body = {
+    size: 200,
+    from: 0, 
+    query: {
+      match: {
+          name: req.query['q']
+      }
+    }
+  }
+  // perform the actual search passing in the index, the search query and the type
+  elastic.search({index:'hafidsquid-2019.08.24',  body:body})
+  .then(results => {
+    res.send(results.hits.hits);
+  })
+  .catch(err=>{
+    console.log(err)
+    res.send([]);
+  });
+
+})
+
+
+
 
 module.exports = router;
 
