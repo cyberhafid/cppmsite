91c7e4fde34d1c156b27dc1613d5a1f4ab81a57f
«hafid» <cyberhafid@gmail.com>
2019-09-20 14:15:52 +0200

    verif last push

--- webapp/src/components/navpanel.jsx
+++ webapp/src/components/navpanel.jsx
@@ -11,22 +11,22 @@ export default class NavPanel extends Component {
 			<div>
 				<div className="row">
 				<div className="col-lg-3 col-md-6 col-xs-12">
-						<Card title="MARAUDES" className="bg-success text-white" >
+						<Card title="ERROR" className="bg-success text-white" >
 							<i className="pi pi-fw pi-calendar" ></i> 	<h2>32</h2>
 						</Card>
 					</div>
 					<div className="col-lg-3 col-md-6 col-xs-12">
-						<Card title="COIFFEURS" className="bg-info text-white" >
+						<Card title="LEVEL" className="bg-info text-white" >
 							<i className="pi pi-fw pi-user" ></i> 	<h2>83</h2>
 						</Card>
 					</div>
 					<div className="col-lg-3 col-md-6 col-xs-12">
-						<Card title="PARTICIPANTS" className="bg-danger text-white" >
+						<Card title="BUFFER" className="bg-danger text-white" >
 								<i className="pi pi-fw pi-users" ></i> 	<h2>140</h2>
 						</Card>
 					</div>
 					<div className="col-lg-3 col-md-6 col-xs-12">
-						<Card title="PHOTOS" className="bg-warning text-white" >
+						<Card title="LOGCOLLECTOR" className="bg-warning text-white" >
 							<i className="pi pi-fw pi-users" ></i> 	<h2>637</h2>
 						</Card>
 					</div>
