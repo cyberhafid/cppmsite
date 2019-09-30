9981d1c271ddb4ada7f124c606a26d3d9a44bbbe
cyberhafid <cyberhafid@gmail.com>
2019-09-30 07:39:01 +0200

    add html

--- htmlweb/tableau/axios.js
+++ htmlweb/tableau/axios.js
@@ -2,23 +2,65 @@
      
      function myFunction() {
 
-   
-
-
-        const data = axios.get({
-          baseURL: 'https://api.github.com/',
-          url: "/search/repositories",
-          params: {
-            sort: 'stars',
-            order: 'desc',
-            q: 'language:javascript created:>2018-04-15',
-          }
-        })
+         axios.get('https://pokeapi.co/api/v2/pokemon/', )
+         .then(response => (info = response))
       
-      
-console.log(data)  ;    
+console.log(info)  ;    
   //return items.map(({ id, full_name, html_url, description }) => ({ id, name: full_name, url: html_url, description }));
+    };
 
 
- 
+    function loadPoke() {
+      fetch('https://pokeapi.co/api/v2/pokemon/')
+      .then(function (response) {
+        if (response.ok)
+          return response.json();
+        throw new Error('Response is not OK');
+      })
+      .then(function (data) {
+        console.log(data);
+        data.results.forEach(function (pokemon) {
+          recupInfo(pokemon.url);
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+
+
+
+
+
+    function loadCity() {
+      let displayResources = document.querySelector("#display-resources");
+      displayResources.textContent =
+      "Loading data from JSON source...";
+      fetch("https://geo.api.gouv.fr/departements/13/communes?fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre")
+  
+        .then(function(response) {
+          if (response.ok)
+          return response.json();
+        throw new Error('Response is not OK');
+      })
+        .then(function(result) {
+          let output =
+            "<table class='table table-striped'><thead><tr><th scope='col'>Name</th><th scope='col'>Provider</th><th>URL</th></thead><tbody>";
+          for (let i in result) {
+            output +=
+              "<tr><td>" +
+              result[i].code +
+              "</td><td>" +
+              result[i].nom +
+              "</td><td>" +
+              result[i].population +
+              "</td></tr>";
+          }
+          output += "</tbody></table>";
+          displayResources.innerHTML = output;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     };
--- htmlweb/tableau/source.js
+++ htmlweb/tableau/source.js
@@ -1,27 +1,62 @@
-document.querySelector("#retrieve-resources").onclick = () => {
-    let displayResources = document.querySelector("#display-resources");
-    displayResources.textContent =
-      "Loading data from JSON source...";
-    fetch("resources.json")
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(result) {
-        let output =
-          "<table class='table table-striped'><thead><tr><th scope='col'>Name</th><th scope='col'>Provider</th><th>URL</th></thead><tbody>";
-        for (let i in result) {
-          output +=
-            "<tr><td>" +
-            result[i].name +
-            "</td><td>" +
-            result[i].provider +
-            "</td><td>" +
-            result[i].url +
-            "</td></tr>";
-        }
-        output += "</tbody></table>";
-        displayResources.innerHTML = output;
-      });
-  };
 
- 
+     
+  function myaxiosFunction() {
+
+      axios.get('https://pokeapi.co/api/v2/pokemon/' )
+      .then(response => (info = response))
+   
+   
+return items.map(({ name, url }) => ({ name: name, url: url }));
+
+ };
+
+
+ function loadfetchPoke() {
+   fetch('https://pokeapi.co/api/v2/pokemon/')
+   .then(function (response) {
+     if (response.ok)
+       return response.json();
+     throw new Error('Response is not OK');
+   })
+   .then(function (data) {
+     console.log(data);
+     data.results.forEach(function (pokemon) {
+       recupInfo(pokemon.url);
+     });
+   })
+   .catch(function (error) {
+     console.log(error);
+   });
+ }
+
+ function loadfetchCity() {
+   let displayResources = document.querySelector("#display-resources");
+   displayResources.textContent =
+   "Loading data from JSON source...";
+   fetch("https://geo.api.gouv.fr/departements/13/communes?fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre")
+
+     .then(function(response) {
+       if (response.ok)
+       return response.json();
+     throw new Error('Response is not OK');
+   })
+     .then(function(result) {
+       let output =
+         "<table class='table table-striped'><thead><tr><th scope='col'>Name</th><th scope='col'>Provider</th><th>URL</th></thead><tbody>";
+       for (let i in result) {
+         output +=
+           "<tr><td>" +
+           result[i].code +
+           "</td><td>" +
+           result[i].nom +
+           "</td><td>" +
+           result[i].population +
+           "</td></tr>";
+       }
+       output += "</tbody></table>";
+       displayResources.innerHTML = output;
+     })
+     .catch(function (error) {
+       console.log(error);
+     });
+ };
