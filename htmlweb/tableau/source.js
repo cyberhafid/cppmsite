cbce61bbe146f8acaa1ebe14dab6547a008bf480
cyberhafid <cyberhafid@gmail.com>
2019-10-02 17:08:45 +0200

    add array

--- htmlweb/tableau/source.js
+++ htmlweb/tableau/source.js
@@ -1,62 +1,102 @@
 
-     
-  function myaxiosFunction() {
 
-      axios.get('https://pokeapi.co/api/v2/pokemon/' )
-      .then(response => (info = response))
-   
-   
-return items.map(({ name, url }) => ({ name: name, url: url }));
+function loadfetchDataRefresh() {
+  for (i = 0; i < 5; i++) {
+    clearInterval(i);
+    console.log(i);
+  }
+ let varrefresh = document.getElementById("refresh-dropdown").value;
+  if (varrefresh >= 1) {
+    let timer = setInterval(function () { loadfetchData() }, varrefresh);
+      } 
+}
 
- };
+function loadfetchData() {
+
+  let varservice = document.getElementById("service-dropdown").value;
+  let varpage = document.getElementById("page-dropdown").value;
+  let displayResources = document.querySelector("#display-resources");
+
+  fetch('http://marc.in2p3.fr:8080/api/v0/msgs/' + varservice + '?n=' + varpage)
+    .then(function (response) {
+      if (response.ok)
+        return response.json();
+      throw new Error('Response is not OK');
+    })
+    .then(function (result) {
+
+      console.log(result);
+      let output =
+
+        "<table class='table table-striped table-sm table-bordered'><thead><tr><th class='col-md-2'>date</th><th scope='col' class='col-md-1'>level</th><th scope='col' class='col-md-2'>service</th><th scope='col' class='col-md-7'>message</th></thead><tbody>";
+      for (let i in result) {
+
+        if (result[i].levelname === 'ERROR') { couleur = 'text-danger' } else { couleur = 'text-dark' };
+        var varhost = result[0].host;
+        output +=
+          "<tr class=" + couleur + "><td >" +
+          result[i].asctime +
+          "</td><td >" +
+          result[i].levelname +
+          "</td><td >" +
+          result[i].customname +
+          "</td><td >" +
+          result[i].message + result[i].spacer + result[i].varmessage +
+          "</td></tr>";
+         }
+      output += "</tbody></table>";
+      displayResources.innerHTML = output;
+
+      var txt = document.getElementById('txt');
+      //txt = replace.txt;
+     console.log(varhost);
+      txt.innerHTML = varhost;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
 
- function loadfetchPoke() {
-   fetch('https://pokeapi.co/api/v2/pokemon/')
-   .then(function (response) {
-     if (response.ok)
-       return response.json();
-     throw new Error('Response is not OK');
-   })
-   .then(function (data) {
-     console.log(data);
-     data.results.forEach(function (pokemon) {
-       recupInfo(pokemon.url);
-     });
-   })
-   .catch(function (error) {
-     console.log(error);
-   });
- }
+//var txt = document.getElementById('txt');
+//var inputs = document.getElementsByName('value');
+//txt.innerHTML += inputs[0].value + "<br />";
 
- function loadfetchCity() {
-   let displayResources = document.querySelector("#display-resources");
-   displayResources.textContent =
-   "Loading data from JSON source...";
-   fetch("https://geo.api.gouv.fr/departements/13/communes?fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre")
 
-     .then(function(response) {
-       if (response.ok)
-       return response.json();
-     throw new Error('Response is not OK');
-   })
-     .then(function(result) {
-       let output =
-         "<table class='table table-striped'><thead><tr><th scope='col'>Name</th><th scope='col'>Provider</th><th>URL</th></thead><tbody>";
-       for (let i in result) {
-         output +=
-           "<tr><td>" +
-           result[i].code +
-           "</td><td>" +
-           result[i].nom +
-           "</td><td>" +
-           result[i].population +
-           "</td></tr>";
-       }
-       output += "</tbody></table>";
-       displayResources.innerHTML = output;
-     })
-     .catch(function (error) {
-       console.log(error);
-     });
- };
+
+
+function loadfetchService() {
+  loadfetchDataRefresh();
+  let dropdown = document.getElementById('service-dropdown');
+  dropdown.length = 0;
+
+  let defaultOption = document.createElement('option');
+  defaultOption.text = 'Choose Service';
+
+  dropdown.add(defaultOption);
+  dropdown.selectedIndex = 0;
+
+  const url = 'http://marc.in2p3.fr:8080/api/v0/services';
+  fetch(url)
+    .then(
+      function (response) {
+        if (response.status !== 200) {
+          console.warn('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+        response.json().then(function (data) {
+          let option;
+          for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option');
+            option.text = data[i];
+            option.value = data[i];
+            dropdown.add(option);
+          }
+        });
+      }
+    )
+    .catch(function (err) {
+      console.error('Fetch Error -', err);
+    });
+}
